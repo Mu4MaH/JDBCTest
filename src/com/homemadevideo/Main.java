bfa43bc0de0a4b3ef190f9fe7e69951202d69c95
Mu4MaH <mu4mah@gmail.com>
2018-03-16 15:48:10 +0300

    Prep. Stat.

--- src/com/homemadevideo/Main.java
+++ src/com/homemadevideo/Main.java
@@ -16,14 +16,25 @@ public class Main {
         try (Connection connection = DriverManager.getConnection(connectionUrl, username, passw); //передаётся адрес базы, логин, пароль
              Statement statement = connection.createStatement())        {
             //statement.executeUpdate("CREATE TABLE playlist (id INT AUTO_INCREMENT NOT NULL , song CHAR(50) NOT NULL , author CHAR(30), PRIMARY KEY (id))");
-            statement.executeUpdate("INSERT INTO playlist (song, author) VALUES ('Succubus', '5FDP')");
-            statement.executeUpdate("INSERT INTO users SET name='Ivan' , pass = '0000'");
-
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM playlist");
+            String songId = "1";
+            String tableName = "playlist";
+//            statement.executeUpdate("INSERT INTO playlist (song, author) VALUES ('Succubus', '5FDP')");
+//            statement.executeUpdate("INSERT INTO users SET name='Ivan' , pass = '0000'");
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM playlist");
+//            while (resultSet.next()) {
+//                System.out.print(resultSet.getString(3) + " " + resultSet.getString(2)); System.out.println();
+//            }
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM ? WHERE id>1");
+            //preparedStatement.setString(1, tableName );
+            preparedStatement.setString(1, tableName);
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                System.out.print(resultSet.getString(3) + " " + resultSet.getString(2)); System.out.println();
+                System.out.println(resultSet.getString(2) + " " + resultSet.getString("author"));
             }
-            //statement.execute("DELETE FROM playlist WHERE id > 1");
+            
+
+
+
         }
     }
 }
